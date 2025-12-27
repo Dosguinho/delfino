98e1d58c12702c41157670040f3550b74707f8ed
delfino <105988154+Dosguinho@users.noreply.github.com>
2025-12-27 21:59:15 +0000

    Fix: ajustando base path e arquivos de build

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: "/delfino/", // ISSO AQUI É O MAIS IMPORTANTE
-  server: {
-    host: "0.0.0.0",
-    port: 8080,
-  },
+  base: "/delfino/", 
   plugins: [react()],
   resolve: {
     alias: {
