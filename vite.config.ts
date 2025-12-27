498c1dcd23ebb91aacf0e3a41bbb97f014682cec
delfino <105988154+Dosguinho@users.noreply.github.com>
2025-12-27 22:25:00 +0000

    Versão simplificada sem erros

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: "/delfino/", // Adicione isso para o GitHub Pages reconhecer os caminhos
+  base: "./", 
   plugins: [react()],
   resolve: {
     alias: {
